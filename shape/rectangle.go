349905d5e83aba2e3d108c394ce798cecefdb588
gcprod <77842240+gcprod@users.noreply.github.com>
2025-01-04 11:14:06 +0700

    Update rectangle.go

--- shape/rectangle.go
+++ shape/rectangle.go
@@ -2,12 +2,12 @@ package shape
 
 type Rectangle struct {
 	Width  float64
-	Height float64
+	Length float64
 }
 
 func (rectangle *Rectangle) Area() float64 {
-	return rectangle.Width * rectangle.Height
+	return rectangle.Width * rectangle.Length
 }
 func (rectangle *Rectangle) Perimeter() float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+	return 2 * (rectangle.Width + rectangle.Length)
 }
