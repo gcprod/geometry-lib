007e26a933180f4e1321d5d1071395ba9781ae3f
Gilang <gilangcpurnaA@gmail.com>
2025-01-04 11:07:23 +0700

    update

---
+++ shape/rectangle.go
@@ -0,0 +1,13 @@
+package shape
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (rectangle *Rectangle) Area() float64 {
+	return rectangle.Width * rectangle.Height
+}
+func (rectangle *Rectangle) Perimeter() float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
